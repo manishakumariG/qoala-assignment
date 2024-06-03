4b5d0bc89b9854748af561e43ab46d84e4e25111
manisha2_gupta <manisha2.gupta@paytm.com>
2024-06-03 19:27:27 +0530

    Updating the changes

--- assingment/task_1/terraform/environment/test-ec2/backend.tf
+++ assingment/task_1/terraform/environment/test-ec2/backend.tf
@@ -1,11 +1,8 @@
 //Stores the state file on s3 bucket
 terraform {
   backend "s3" {
-    #bucket         = "s3-qoala-tfstates"
-    bucket         = "ppsl-ap-south-1-tfstates"
-    key            = "vpc/production/ppsl-prod-vpc/ec2-test.tfstate"
-    #key            = "ec2/testing/qoala/ec2-test.tfstate"
+    bucket         = "s3-qoala-tfstates"
+    key            = "ec2/testing/qoala/ec2-test.tfstate"
     region         = "ap-south-1"
-    profile        = "aws-ppsl-datalake-admin"
   }
 }
